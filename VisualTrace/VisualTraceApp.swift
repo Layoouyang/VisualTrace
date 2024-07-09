b3a5eb9be35bc89383d5f0a706b644c4f96d126b
Leo Ouyang <layoouyang@gmail.com>
2024-07-08 22:23:02 -0400

    Initial Commit

---
+++ VisualTrace/VisualTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualTraceApp.swift
+//  VisualTrace
+//
+//  Created by Leo Ouyang on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
